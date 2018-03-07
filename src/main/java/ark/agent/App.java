79106fc01532be76fe637122e984e0ace284b14b
Ark <arktheprogrammer@gmail.com>
2018-03-07 10:36:37 -0500

    Load up akka classes

--- src/main/java/ark/agent/App.java
+++ src/main/java/ark/agent/App.java
@@ -1,7 +1,21 @@
 package ark.agent;
 
 public class App {
-  public static void main(String[] args) {
+  public static final String[] CLASSNAMES = {
+    "akka.dispatch.Mailbox",
+    "akka.dispatch.forkjoin.ForkJoinTask",
+    "akka.dispatch.Mailboxes$$anon$1",
+    "akka.dispatch.forkjoin.ForkJoinTask$AdaptedRunnableAction",
+    "akka.dispatch.forkjoin.ForkJoinTask$AdaptedCallable",
+    "akka.dispatch.forkjoin.ForkJoinTask$AdaptedRunnable",
+    "akka.dispatch.Dispatcher$$anon$1",
+    "akka.dispatch.ForkJoinExecutorConfigurator$AkkaForkJoinTask"
+  };
+
+  public static void main(String[] args) throws Exception {
     System.out.println("Enter Main");
+    for (int i = 0; i < CLASSNAMES.length; ++i) {
+      System.out.println("Load class " + Class.forName(CLASSNAMES[i]));
+    }
   }
 }

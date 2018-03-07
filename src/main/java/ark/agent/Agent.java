ca6516050620338611766c97864f337901928cb4
Ark <arktheprogrammer@gmail.com>
2018-03-07 10:16:32 -0500

    Install agent

--- src/main/java/ark/agent/Agent.java
+++ src/main/java/ark/agent/Agent.java
@@ -1,9 +1,40 @@
 package ark.agent;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.utility.JavaModule;
+
 import java.lang.instrument.Instrumentation;
 
 public class Agent {
     public static void premain(final String agentArgs, final Instrumentation inst) throws Exception {
         System.out.println("Enter premain");
+        AgentBuilder agentBuilder =
+                new AgentBuilder.Default()
+                        .disableClassFormatChanges()
+                        .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+                        .with(new LoggingListener());
+        agentBuilder.installOn(inst);
+    }
+
+    public static class LoggingListener implements AgentBuilder.Listener {
+        @Override
+        public void onDiscovery(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+            // System.out.println("Discovered: " + typeName);
+        }
+        @Override
+        public void onTransformation(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded, DynamicType dynamicType) {
+            System.out.println("Transformed: " + typeDescription + " :: " + classLoader);
+        }
+        @Override
+        public void onIgnored(TypeDescription typeDescription, ClassLoader classLoader, JavaModule module, boolean loaded) {
+        }
+        @Override
+        public void onError(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded, Throwable throwable) {
+        }
+        @Override
+        public void onComplete(String typeName, ClassLoader classLoader, JavaModule module, boolean loaded) {
+        }
     }
 }

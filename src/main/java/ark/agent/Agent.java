647fbc2697e6007bfd6ea5e4360d46ad51d8722f
Ark <arktheprogrammer@gmail.com>
2018-03-07 10:11:33 -0500

    Add agent to run task

--- src/main/java/ark/agent/Agent.java
+++ src/main/java/ark/agent/Agent.java
@@ -1,4 +1,9 @@
 package ark.agent;
 
+import java.lang.instrument.Instrumentation;
+
 public class Agent {
+    public static void premain(final String agentArgs, final Instrumentation inst) throws Exception {
+        System.out.println("Enter premain");
+    }
 }
--- src/main/java/ark/agent/App.java
+++ src/main/java/ark/agent/App.java
@@ -2,6 +2,6 @@ package ark.agent;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    System.out.println("Enter Main");
   }
 }
